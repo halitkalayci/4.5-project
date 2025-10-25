85bd6974d13973cdf29276ab92072aa55ec40a91
halitklyc <halitenes.kalayci@gmail.com>
2025-10-25 15:54:25 +0300

    Refine order creation event handling in OrdersController by using MessageBuilder for Kafka message construction. Removed unnecessary whitespace in docker-compose.yml.

--- order-service/src/main/java/com/turkcell/order_service/controller/OrdersController.java
+++ order-service/src/main/java/com/turkcell/order_service/controller/OrdersController.java
@@ -3,6 +3,8 @@ package com.turkcell.order_service.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -17,9 +19,13 @@ public class OrdersController {
 
     @PostMapping()
     public String createOrder(@RequestBody CreateOrderDto dto) {
+
         OrderCreatedEvent event = new OrderCreatedEvent(dto.productId());
-        streamBridge.send("orderCreated-out-0", event);
-        // Kafka eventi fırlat..
+
+        Message<OrderCreatedEvent> message = MessageBuilder.withPayload(event).build();
+
+        streamBridge.send("orderCreated-out-0", message);
+
         return dto.productId();
     }
 
