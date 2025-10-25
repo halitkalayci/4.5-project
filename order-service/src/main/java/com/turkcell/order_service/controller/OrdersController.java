f43c5ed388c4298f3de7e1053b3e6200b581e935
halitklyc <halitenes.kalayci@gmail.com>
2025-10-25 15:44:05 +0300

    Refactor Kafka setup in docker-compose and enhance order-service with event publishing. Updated Kafka ports and added network configuration in docker-compose.yml. Modified OrdersController to publish order creation events and adjusted application.yml for new Kafka broker settings.

--- order-service/src/main/java/com/turkcell/order_service/controller/OrdersController.java
+++ order-service/src/main/java/com/turkcell/order_service/controller/OrdersController.java
@@ -2,18 +2,30 @@ package com.turkcell.order_service.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/v1/orders")
 public class OrdersController {
+    private final StreamBridge streamBridge;
+
+    public OrdersController(StreamBridge streamBridge) {
+        this.streamBridge = streamBridge;
+    }
+
     @PostMapping()
     public String createOrder(@RequestBody CreateOrderDto dto) {
+        OrderCreatedEvent event = new OrderCreatedEvent(dto.productId());
+        streamBridge.send("orderCreated-out-0", event);
         // Kafka eventi fırlat..
-        return dto.productId;
+        return dto.productId();
     }
 
     record CreateOrderDto(String productId) {
+    }
+
+    record OrderCreatedEvent(String productId) {
     }
 }
