d755e2a13e32e9846b68e9d633fa7909c7d96cab
halitklyc <halitenes.kalayci@gmail.com>
2025-10-26 11:53:47 +0300

    outbox impl.

--- order-service/src/main/java/com/turkcell/order_service/controller/OrdersController.java
+++ order-service/src/main/java/com/turkcell/order_service/controller/OrdersController.java
@@ -2,6 +2,14 @@ package com.turkcell.order_service.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.turkcell.order_service.messaging.outbox.OutboxMessage;
+import com.turkcell.order_service.messaging.outbox.OutboxRepository;
+
+import java.util.UUID;
+
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
@@ -11,26 +19,38 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequestMapping("/api/v1/orders")
 public class OrdersController {
-    private final StreamBridge streamBridge;
+    private final OutboxRepository outboxRepository;
+    private final ObjectMapper objectMapper;
 
-    public OrdersController(StreamBridge streamBridge) {
-        this.streamBridge = streamBridge;
+    public OrdersController(OutboxRepository outboxRepository, ObjectMapper objectMapper) {
+        this.outboxRepository = outboxRepository;
+        this.objectMapper = objectMapper;
     }
 
     @PostMapping()
-    public String createOrder(@RequestBody CreateOrderDto dto) {
+    public String createOrder(@RequestBody CreateOrderDto dto) throws JsonProcessingException {
         /// .........
         OrderCreatedEvent event = new OrderCreatedEvent(dto.productId());
 
-        Message<OrderCreatedEvent> message = MessageBuilder.withPayload(event).build();
+        OutboxMessage outboxMessage = new OutboxMessage();
+        outboxMessage.setAggregateId(UUID.randomUUID()); // normalde db'de oluşan order'ın idsi
+        outboxMessage.setAggregateType("Order");
+        outboxMessage.setEventId(UUID.randomUUID());
+        outboxMessage.setEventType("OrderCreatedEvent");
+        outboxMessage.setPayloadJson(objectMapper.writeValueAsString(event));
+        outboxRepository.save(outboxMessage);
 
-        try {
-            boolean isSent = streamBridge.send("orderCreated-out-0", message);
-            if (!isSent)
-                System.out.println("Mesaj gönderilemedi");
-        } catch (Exception e) {
-            System.out.println("Mesaj gönderilemedi");
-        }
+        // Message<OrderCreatedEvent> message =
+        // MessageBuilder.withPayload(event).build();
+        /*
+         * try {
+         * boolean isSent = streamBridge.send("orderCreated-out-0", message);
+         * if (!isSent)
+         * System.out.println("Mesaj gönderilemedi");
+         * } catch (Exception e) {
+         * System.out.println("Mesaj gönderilemedi");
+         * }
+         */
 
         return dto.productId();
     }
@@ -38,6 +58,6 @@ public class OrdersController {
     record CreateOrderDto(String productId) {
     }
 
-    record OrderCreatedEvent(String productId) {
+    public record OrderCreatedEvent(String productId) {
     }
 }
